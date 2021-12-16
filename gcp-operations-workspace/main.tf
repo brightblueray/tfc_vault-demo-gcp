64a767dffdb5e98a3d81378c6c673395bc8ded2b
brightblueray <rryjewski@hashicorp.com>
2021-12-16 08:27:16 -0500

    final

--- gcp-operations-workspace/main.tf
+++ gcp-operations-workspace/main.tf
@@ -1,7 +1,7 @@
 # Make sure you grab a vault token
 
 terraform {
-  # Leverage TFC for state management and run execution
+ # Leverage TFC for state management and run execution
   cloud {
     organization = "hashicorp-rryjewski"
     workspaces {
@@ -23,12 +23,9 @@ terraform {
 # Created a temp GCP Project and copy details
 locals {
   projectId = "hc-d7442f1a33034ed6b588e77e0dc"
-  project = "rryjewski-gcpdemo-test"
 }
 
-variable "name" { default = "dynamic-gcp-creds-demo" }
-variable "rryjewski_sa_creds" {}
-variable "vault_user" { default = "rryjewski" }
+variable "vault_user" {}
 variable "vault_pwd" {}
 
 # Using Hashicorp Cloud Platform (HCP) Vault
@@ -41,50 +38,23 @@ provider "vault" {
   }
 }
 
-# Manually created a service account to be used to manage the temporary oath tokens
-resource "vault_gcp_secret_backend" "gcp" {
-  credentials = var.rryjewski_sa_creds
-  path       = "${var.name}-path"
-
-  default_lease_ttl_seconds = "120"
-  max_lease_ttl_seconds     = "240"
-}
-
-#
-resource "vault_gcp_secret_roleset" "infra-roleset" {
-  backend = vault_gcp_secret_backend.gcp.path
-  roleset = "gcp-builder-roleset"
-  secret_type = "access_token"
-  project = local.projectId
-  token_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
-  binding {
-    resource = "//cloudresourcemanager.googleapis.com/projects/${local.projectId}"
-
-    roles = [
-      "roles/compute.admin",
-    ]
-  }
-}
-
-
-
 provider "google" {
-  project = "hc-d7442f1a33034ed6b588e77e0dc"
+  project = local.projectId
   region  = "us-central1"
   zone    = "us-central1-c"
   access_token = data.vault_generic_secret.gcp_auth.data["token"]
 }
 
 data "vault_generic_secret" "gcp_auth" {
-  path = "gcp/roleset/example-roleset/token"
+  path = "dynamic-gcp-creds-demo-path/roleset/gcp-builder-roleset/token"
 }
 
 resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+  name = "terraform-network-rryjewski"
 }
 
 resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
+  name         = "terraform-instance-rryjewski"
   machine_type = "f1-micro"
   tags = ["web", "dev", "temp"]
 
--- outputs.tf
+++
@@ -1,7 +0,0 @@
-output "backend" {
-  value = vault_gcp_secret_backend.gcp.path
-}
-
-output "roleset" {
-  value = vault_gcp_secret_roleset.infra-roleset.roleset
-}
